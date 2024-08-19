24f7c1c597626aef50b4f2b429779e6faaf650aa
Kanchan Vidhate <kvidhate34@gmail.com>
2024-08-19 16:49:23 +0530

    init commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+const PORT=process.env.PORT ||5000;
+const MONGO_URL=process.env.MONGO_URL;
+
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+
+//EFFIE
+(async()=>{
+    const conn=await mongoose.connect(MONGO_URL);
+
+     if(conn){
+        console.log('connect to MongoDB');
+
+     }
+    })();
+    //
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+
+});
+
